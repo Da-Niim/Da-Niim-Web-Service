984ca0c60e1addc05e5113396928c7c0be9e2d3f
Lee MinGi <mingi3442@gmail.com>
2023-11-17 17:03:03 +0900

    #DAN-14/Fix: 중첩 CSS Warning 해결 및 지도 애니메이션 제거

--- src/components/main/molecules/MainKoreaAdministrativeDistrictSvg.tsx
+++ src/components/main/molecules/MainKoreaAdministrativeDistrictSvg.tsx
@@ -1,13 +1,15 @@
 "use client"
+/** @jsxImportSource @emotion/react */
 import { css } from "@emotion/css"
 import { koreaAdministrativeDistrictData } from "../constant"
+import MainKoreaSvg from "../atoms/MainKoreaSvg"
 
 export default function MainKoreaAdministrativeDistrictSvg() {
   const handleClick = (event: React.MouseEvent<SVGPathElement>) => {
     console.log(`Clicked path: ${event.currentTarget.getAttribute("name")}`)
   }
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 524 631" aria-label="Map of South Korea">
+    <MainKoreaSvg>
       {koreaAdministrativeDistrictData.map((path, index) => {
         return (
           <path
@@ -17,15 +19,14 @@ export default function MainKoreaAdministrativeDistrictSvg() {
             name={path.name}
             onClick={handleClick}
             className={css`
-              fill: 2c2c2c;
-              stroke: blue;
               &:hover {
-                fill: #ffffff;
+                fill: #d2b48c;
+                box-shadow: 0 10px 20px rgba(0, 0, 0, 0.3);
               }
             `}
           />
         )
       })}
-    </svg>
+    </MainKoreaSvg>
   )
 }
--- src/components/main/organisms/MainKoreaMapContainer.tsx
+++ src/components/main/organisms/MainKoreaMapContainer.tsx
@@ -1,8 +1,8 @@
-import MainKoreaAdministrativeDistrictSvg from "../atoms/MainKoreaAdministrativeDistrictSvg"
+import MainKoreaAdministrativeDistrictSvg from "../molecules/MainKoreaAdministrativeDistrictSvg"
 
 export default function MainKoreaMapContainer() {
   return (
-    <div className="w-[32rem] h-auto">
+    <div className="w-[32rem] h-auto m-4">
       <MainKoreaAdministrativeDistrictSvg />
     </div>
   )
