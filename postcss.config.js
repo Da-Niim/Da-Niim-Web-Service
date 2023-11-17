ef8c38f285400caa76a6d65b49b139ace98cdbff
sunwoo020 <sunwoo020@naver.com>
2023-11-17 18:53:35 +0900

    DAN-12/feat: Loading  구현

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+    "postcss-preset-env": {
+      stage: 3,
+      features: {
+        "nesting-rules": true,
+      },
+    },
   },
 }
