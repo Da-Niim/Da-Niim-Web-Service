66322889ca4ec2549ac4a9dbbea29cb8a25ccfc8
Lee MinGi <mingi3442@gmail.com>
2023-12-15 07:38:40 +0900

    #DAN-21/Feat: 지도 행정구역 Hover할 경우 이벤트 추가

--- src/components/main/molecules/MainKoreaAdministrativeDistrictSvg.tsx
+++ src/components/main/molecules/MainKoreaAdministrativeDistrictSvg.tsx
@@ -3,30 +3,69 @@
 import { css } from "@emotion/css"
 import { KOREA_ADMINISTRATIVE_DISTRICT_DATA } from "../constant"
 import MainKoreaSvg from "../atoms/MainKoreaSvg"
+import { useState } from "react"
+import { Tooltip as ReactTooltip } from "react-tooltip"
+import { SvgKoreaAdministrativeDistrictNameType } from "../utils/types"
 
 export default function MainKoreaAdministrativeDistrictSvg() {
-  const handleClick = (event: React.MouseEvent<SVGPathElement>) => {
-    console.log(`Clicked path: ${event.currentTarget.getAttribute("name")}`)
+  const [hoveredRegion, setHoveredRegion] = useState<SvgKoreaAdministrativeDistrictNameType | null>(null)
+
+  const handleMouseEnter = (event: any, path: any) => {
+    setHoveredRegion(path.name)
   }
+
+  const handleMouseLeave = () => {
+    setHoveredRegion(null)
+  }
+  // const handleClick = (event: React.MouseEvent<SVGPathElement>) => {
+  //   console.log(`Clicked path: ${event.currentTarget.getAttribute("name")}`)
+  // }
+
   return (
-    <MainKoreaSvg>
-      {KOREA_ADMINISTRATIVE_DISTRICT_DATA.map((path, index) => {
-        return (
-          <path
-            key={index}
-            d={path.pathData}
-            id={path.id}
-            name={path.name}
-            onClick={handleClick}
-            className={css`
-              &:hover {
-                fill: #d2b48c;
-                box-shadow: 0 10px 20px rgba(0, 0, 0, 0.3);
-              }
-            `}
-          />
-        )
-      })}
-    </MainKoreaSvg>
+    <>
+      <MainKoreaSvg>
+        {KOREA_ADMINISTRATIVE_DISTRICT_DATA.map((path, index) => {
+          return (
+            <g key={index}>
+              <path
+                // onMouseOver={onMouseHover}
+                data-tooltip-id="location-tooltip"
+                d={path.pathData}
+                id={path.id}
+                name={path.name}
+                className={css`
+                  &:hover {
+                    fill: #d2b48c;
+                    box-shadow: 0 10px 20px rgba(0, 0, 0, 0.3);
+                    z-index: -1;
+                  }
+                `}
+                onMouseEnter={(e) => handleMouseEnter(e, path)}
+              />
+            </g>
+          )
+        })}
+      </MainKoreaSvg>
+      <ReactTooltip
+        style={{
+          marginTop: `${
+            hoveredRegion === "경상북도" || hoveredRegion === "강원도" || hoveredRegion === "경기도"
+              ? "10vh"
+              : hoveredRegion === "전라북도" ||
+                hoveredRegion === "전라남도" ||
+                hoveredRegion === "경상남도" ||
+                hoveredRegion === "충청남도" ||
+                hoveredRegion === "충청북도"
+              ? "5vh"
+              : "0"
+          }`,
+          border: "1px solid #2c2c2c",
+        }}
+        id="location-tooltip"
+        place="top"
+        variant="success"
+        content={`${hoveredRegion}`}
+      />
+    </>
   )
 }
