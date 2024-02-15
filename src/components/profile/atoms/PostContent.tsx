74e91652a9567abbb419403d6b60c62bc7c238c0
Lee MinGi <mingi3442@gmail.com>
2024-02-15 14:16:31 +0900

    #DAN-43/Refactor: 누락된 CSS 수정

--- src/components/profile/atoms/PostContent.tsx
+++ src/components/profile/atoms/PostContent.tsx
@@ -1,6 +1,6 @@
 const PostContent = () => {
   return (
-    <div className="hover:cursor-pointer hover:opacity-80 w-72 bg-blue-500 h-72 rounded-md border border-[#D9D9D9"></div>
+    <div className="hover:cursor-pointer hover:opacity-80 w-72 bg-blue-500 h-72 rounded-md border border-[#D9D9D9]"></div>
   )
 }
 
