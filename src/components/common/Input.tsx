f2e9439b9e418b93e688609d1193557c0723bd18
Lee MinGi <mingi3442@gmail.com>
2024-04-03 02:14:15 +0900

    #DAN-49/Style: Input Focus시 border 색상 적용용

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -6,7 +6,15 @@ interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
 }
 
 const Input: React.FC<InputProps> = ({ className, ...props }) => {
-  return <input {...props} className={ctm(`p-4 border border-gray-500 rounded-lg font-normal w-full`, className)} />
+  return (
+    <input
+      {...props}
+      className={ctm(
+        `p-4 border border-gray-500 rounded-lg font-normal w-full focus:outline-none focus:border-essential-500 focus:border`,
+        className,
+      )}
+    />
+  )
 }
 
 export default Input
